4bf1ccb0ff0affb1eff24aab48ce9c902fab33d6
Kartik3012 <kartik001srivastav@gmail.com>
2022-07-22 22:43:42 +0530

    Updates

---
+++ precache-manifest.a058bcc37728146f5e593a7bb0bb26ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93010b4f1d21a9e70d68",
+    "url": "/Text_Utils-React/static/css/main.c6e39f1a.chunk.css"
+  },
+  {
+    "revision": "93010b4f1d21a9e70d68",
+    "url": "/Text_Utils-React/static/js/main.93010b4f.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text_Utils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text_Utils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "bb39e8c0a95a370c18e2",
+    "url": "/Text_Utils-React/static/js/runtime~main.bb39e8c0.js"
+  },
+  {
+    "revision": "c1897c0fc8fb006c540c263cc7ae725b",
+    "url": "/Text_Utils-React/index.html"
+  }
+];
